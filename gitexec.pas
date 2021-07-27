b8e2483e16fb18e36c69b33a6ff9a66abd4736c1
Dmitry Boyarintsev <skalogryz.lists@gmail.com>
2021-07-27 17:09:53 -0400

    updating the exec unit

--- gitexec.pas
+++ gitexec.pas
@@ -2,11 +2,38 @@ unit gitexec;
 
 interface
 
+uses
+  process,
+  processunicode;
+
 const
-  GitExec = 'git';
   GitLog = 'log';
   GitNoPager = '--no-pager';
 
+type
+  TRunResult = record
+    ExitCode : Integer;
+    StdOut   : string;
+    StdErr   : string;
+  end;
+
+var
+  GitExecPath : TProcessString = 'git'; // should be full path on Linux
+
+procedure GitRun(const dir: UnicodeString;
+  const args: array of UnicodeString; out res: TRunResult);
+
 implementation
+
+procedure GitRun(const dir: UnicodeString;
+  const args: array of UnicodeString; out res: TRunResult);
+var
+  opt : TProcessOptions;
+  st  : string;
+begin
+  res.StdErr:='';
+  opt := [poUsePipes, poWaitOnExit];
+  RunCommandIndir( dir, GitExecPath, args, res.StdOut, res.ExitCode, opt, swoNone);
+end;
 
 end.
